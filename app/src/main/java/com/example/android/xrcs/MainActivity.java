1cfb8249ab2d94a655ff944662688b3a00ece78f
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2018-03-18 18:28:23 +0800

    designed bottom buttons

--- app/src/main/java/com/example/android/xrcs/MainActivity.java
+++ app/src/main/java/com/example/android/xrcs/MainActivity.java
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText("View 1");
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText("View 2");
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText("View 3");
                     return true;
             }
             return false;
