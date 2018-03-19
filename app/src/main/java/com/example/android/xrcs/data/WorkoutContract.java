859bb60a40524dd328e04a839a0591c60d3fea05
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2018-03-19 09:13:41 +0800

    added database contract & dbhelper

---
+++ app/src/main/java/com/example/android/xrcs/data/WorkoutContract.java
@@ -0,0 +1,20 @@
+package com.example.android.xrcs.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Sean on 18-Mar-18.
+ */
+
+public class WorkoutContract {
+    public static final class WorkoutEntry implements BaseColumns{
+        public static final String TABLE_NAME = "workouts";
+        public static final String COLUMN_EXERCISE_NAME =  "exerciseName";
+        public static final String COLUMN_EXERCISE_TYPE =  "exerciseType";
+        public static final String COLUMN_NO_SETS = "noOfSets";
+        public static final String COLUMN_BREAK_TIME = "breakTimeBetweenSets";
+        public static final String COLUMN_REPS = "repsPerSet";
+        public static final String COLUMN_TIMED_TARGET_MODE = "timedTargetModeBool";
+        public static final String COLUMN_TARGET_TIME = "targetTime";
+    }
+}
---
+++ app/src/main/java/com/example/android/xrcs/data/WorkoutDbHelper.java
@@ -0,0 +1,41 @@
+package com.example.android.xrcs.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Sean on 18-Mar-18.
+ */
+
+public class WorkoutDbHelper extends SQLiteOpenHelper{
+    private static final String DATABASE_NAME = "workouts.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public WorkoutDbHelper (Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_WORKOUTS_TABLE = "CREATE TABLE" +
+                WorkoutContract.WorkoutEntry.TABLE_NAME + "(" +
+                WorkoutContract.WorkoutEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                WorkoutContract.WorkoutEntry.COLUMN_EXERCISE_NAME + " TEXT NOT NULL, " +
+                WorkoutContract.WorkoutEntry.COLUMN_EXERCISE_TYPE + " TEXT NOT NULL, " +
+                WorkoutContract.WorkoutEntry.COLUMN_NO_SETS + " INTEGER NOT NULL, " +
+                WorkoutContract.WorkoutEntry.COLUMN_NO_SETS + " INTEGER NOT NULL, " +
+                WorkoutContract.WorkoutEntry.COLUMN_BREAK_TIME + " INTEGER NOT NULL, " +
+                WorkoutContract.WorkoutEntry.COLUMN_REPS + " INTEGER NOT NULL, " +
+                WorkoutContract.WorkoutEntry.COLUMN_TIMED_TARGET_MODE + " TEXT NOT NULL, " +
+                WorkoutContract.WorkoutEntry.COLUMN_TARGET_TIME + " INTEGER NOT NULL, " +
+                " TIMESTAMP DEFAULT CURRENT_TIMESTAMP);";
+        sqLiteDatabase.execSQL(SQL_CREATE_WORKOUTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WorkoutContract.WorkoutEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
