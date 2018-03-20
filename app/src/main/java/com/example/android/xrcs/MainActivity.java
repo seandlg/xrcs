2a5c4817462f55ac01d03a4cb92f0987e971ba57
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2018-03-20 17:47:04 +0800

    updated recycler view and manage workouts fragment design

--- app/src/main/java/com/example/android/xrcs/MainActivity.java
+++ app/src/main/java/com/example/android/xrcs/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.android.xrcs;
 
-import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,10 +8,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private FragmentManager fragmentManager = getSupportFragmentManager();
+    private ActionBar customActionBar;
+    private TextView ActionBarHeadingTV;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,16 +23,19 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_work_out:
                     WorkOutFragment fragment1 = new WorkOutFragment();
+                    ActionBarHeadingTV.setText("Work out");
                     fragmentTransaction.replace(R.id.fragment_container, fragment1).commit();
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_manage_workouts:
                     ManageWorkoutsFragment fragment2 = new ManageWorkoutsFragment();
+                    ActionBarHeadingTV.setText("Manage workouts");
                     fragmentTransaction.replace(R.id.fragment_container, fragment2).commit();
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_stats:
                     StatsFragment fragment3 = new StatsFragment();
+                    ActionBarHeadingTV.setText("Stats");
                     fragmentTransaction.replace(R.id.fragment_container, fragment3).commit();
                     return true;
             }
@@ -40,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Use a custom action bar
+        customActionBar = getSupportActionBar();
+        customActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        customActionBar.setCustomView(R.layout.action_bar_layout);
+        ActionBarHeadingTV = (TextView) findViewById(R.id.action_bar_title);
+        // Set main layout
         setContentView(R.layout.activity_main);
 
         // Set up the bottom navigation
