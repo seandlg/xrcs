484137903712c69fedf999a8873f95a0b5341d0b
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2018-03-22 13:43:12 +0800

    added up button from editWorkoutActivity back 2 MainActivity

--- app/src/main/java/com/example/android/xrcs/editWorkoutActivity.java
+++ app/src/main/java/com/example/android/xrcs/editWorkoutActivity.java
@@ -9,5 +9,6 @@ public class editWorkoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_workout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
